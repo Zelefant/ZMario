4ad67fda0f4a2cc7a3639eca0d68f6b0afa76cf4
Zelefant <zelefantplaysmc@outlook.com>
2026-07-19 11:42:12 -0500

    Debug logger implementation

---
+++ ZMario/Engine/DebugLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMario.Engine
+{
+    internal class DebugLogger : ILogger
+    {
+        public string Name => "DEBUG";
+
+        public ConsoleColor color => ConsoleColor.White;
+
+        public bool Debug
+        {
+            get; set;
+        }
+
+        public void Log(string message)
+        {
+            if (Debug)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine("[" + this.Name + "] " + message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}

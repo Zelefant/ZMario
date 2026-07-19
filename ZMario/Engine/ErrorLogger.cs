29cebe04406b10767a148cee3cf4e4433d3e817d
Zelefant <zelefantplaysmc@outlook.com>
2026-07-19 11:40:54 -0500

    Error logger implementation

---
+++ ZMario/Engine/ErrorLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMario.Engine
+{
+    internal class ErrorLogger : ILogger
+    {
+        public string Name => "ERROR";
+
+        public ConsoleColor color => ConsoleColor.Red;
+
+        public void Log(string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine("[" + this.Name + "] " + message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}

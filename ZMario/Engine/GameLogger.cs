c5c7de19ebf34402c9f30d144990295937ed576f
Zelefant <zelefantplaysmc@outlook.com>
2026-07-19 11:37:06 -0500

    Implement basic game logger.

---
+++ ZMario/Engine/GameLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMario.Engine
+{
+    internal class GameLogger : ILogger
+    {
+        public string Name => "GAME";
+
+        public ConsoleColor color => ConsoleColor.White;
+
+        public void Log(string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}

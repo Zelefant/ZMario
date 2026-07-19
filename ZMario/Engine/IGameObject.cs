5e18e448bbec250e2afb5707afc7dd4474f8beab
Zelefant <zelefantplaysmc@outlook.com>
2026-07-19 10:40:01 -0500

    IGameObject interface method definitions

--- ZMario/Engine/IGameObject.cs
+++ ZMario/Engine/IGameObject.cs
@@ -8,7 +8,13 @@ namespace ZMario.Engine
 {
     internal interface IGameObject
     {
+        // Initialization method for when the object is created or, if exists at scene start, when the scene is fully loaded.
+        void Initialize();
 
+        // Update method that is called every frame.
+        // Delta Time should be passed every time, it is up to the game object if it uses it.
+        // Delta Time is used to untie object updations from the frame rate.
+        void Update(float deltaTime);
 
     }
 }
